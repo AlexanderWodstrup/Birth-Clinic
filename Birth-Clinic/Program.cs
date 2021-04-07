bdac3abd481c5679870f531cfc7c142b54244d8c
AlexanderWodstrup <Alexander.f.Wodstrup@gmail.com>
2021-04-07 10:24:02 +0200

    Add project files.

---
+++ Birth-Clinic/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Birth_Clinic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
