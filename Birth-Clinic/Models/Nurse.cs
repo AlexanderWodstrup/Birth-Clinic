7859b531398955a7ed273cc891d30dc11978b05e
augusthjerrild <augusthjerrild@gmail.com>
2021-04-13 10:44:50 +0200

    Added clinicians

---
+++ Birth-Clinic/Models/Nurse.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Birth_Clinic.Models
+{
+    public class Nurse
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public double Salary { get; set; }
+        public bool Availability { get; set; }
+        public void SetAvailability(bool state)
+        {
+            Availability = state;
+        }
+
+        public BirthClinic BirthClinic { get; set; }
+    }
+}
