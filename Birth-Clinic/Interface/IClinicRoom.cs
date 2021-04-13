2e1b017c8aa394158735cd97cb86a53595a35983
AlexanderWodstrup <Alexander.f.Wodstrup@gmail.com>
2021-04-13 10:42:21 +0200

    Initial Models

---
+++ Birth-Clinic/Interface/IClinicRoom.cs
@@ -0,0 +1,13 @@
+﻿using Birth_Clinic.Models;
+
+namespace Birth_Clinic.Interface
+{
+    public interface IClinicRoom
+    {
+        public string RoomName { get; set; }
+        public int RoomId { get; set; }
+        public bool Availability { get; set; }
+        public void SetAvailability(bool state);
+        public BirthClinic BirthClinic { get; set; }
+    }
+}
