2ab2c0a710ba5fbcdc196d06486e01ccbefb23fd
AlexanderWodstrup <Alexander.f.Wodstrup@gmail.com>
2021-05-10 16:32:33 +0200

    MongoDB projekt

---
+++ Birth-Clinic-Mongo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Birth_Clinic_Mongo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
